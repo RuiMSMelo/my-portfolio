b6ccc25934a2cf29c0e182ccd7685834e75a568a
RuiMSMelo <ruimsmelo@gmail.com>
2023-08-21 21:08:24 +0100

    small details added

--- src/components/NavbarProjects.jsx
+++ src/components/NavbarProjects.jsx
@@ -1,4 +1,8 @@
 import { Link } from 'react-router-dom'
+import githubImg from '../img/github.svg'
+import linkedinImg from '../img/LinkedIn_icon.svg.png'
+import cvPDF from '../img/RuiMeloCV-M.pdf'
+import cvIcon from '../img/cvicon.png'
 
 function Navbar() {
     return (
@@ -8,6 +12,11 @@ function Navbar() {
             </div>
             <div className='navbar-btns'>
                 <Link to="/projects">My Projects</Link> 
+            </div>
+            <div className='links-div'>
+              <Link to='https://github.com/RuiMSMelo?tab=repositories' target='_blank'><img src={githubImg} className='link-imgs'/></Link>
+              <Link to='https://www.linkedin.com/in/rui-ser%C3%B4dio-melo-a19715276/' target='_blank'><img src={linkedinImg} className='link-imgs'/></Link>
+              <a href={cvPDF} target='_blank' rel='noopener noreferrer' className='cv-btn'><img src={cvIcon} className='link-imgs'/></a>
             </div>
         </div>  
     )
