8a81d0495c18cdcaba17b5360665b52f9ea12a92
RuiMSMelo <ruimsmelo@gmail.com>
2023-08-23 13:28:13 +0100

    changed index.js file

--- src/index.js
+++ src/index.js
@@ -5,13 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
+    <BrowserRouter basename='/'>
+      <SessionContextProvider>
+        <App />
+      </SessionContextProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
+
+
 
 reportWebVitals();
