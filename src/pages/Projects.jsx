cd07b3742c507ca6a7755c1a8aaf04a87b300d1c
RuiMSMelo <ruimsmelo@gmail.com>
2023-09-08 17:09:25 +0100

    minor bugs and typos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,26 +12,26 @@ function Projects() {
             <NavbarProjects/>
             <div className='projects text'>
                 <div className='project1'>
-                    <p>React & Express & MongoDB</p>
-                    <Link to='https://lively-sprite-942da6.netlify.app/' target='_blank'><img src={soundSpaceImg} alt='soundspace-img' className='project-imgs'/></Link>
+                    <p>React & Node.js & MongoDB</p>
+                    <Link to='https://lively-sprite-942da6.netlify.app/' target='_blank' className='project-link'><img src={soundSpaceImg} alt='soundspace-img' className='project-imgs'/></Link>
                     <div className='two-githubs'>
                         <Link to='https://github.com/RuiMSMelo/soundSpaceFrontend/' target='_blank'><img src={githubImg} className='link-imgs' title='frontend' alt=''/></Link>
                         <Link to='https://github.com/RuiMSMelo/soundSpaceBackend/' target='_blank'><img src={githubImg} className='link-imgs' title='backend' alt=''/></Link>
                     </div>
                 </div>
                 <div className='project2'>
-                    <p>Express & MongoDB</p>
-                    <Link to='https://bookzone.adaptable.app/' target='_blank'><img src={bookZoneImg} alt='bookzone-img' className='project-imgs'/></Link>   
+                    <p>Node.js & MongoDB</p>
+                    <Link to='https://bookzone.adaptable.app/' target='_blank' className='project-link'><img src={bookZoneImg} alt='bookzone-img' className='project-imgs'/></Link>   
                     <Link to='https://github.com/RuiMSMelo/bookzone' target='_blank'><img src={githubImg} className='link-imgs' alt=''/></Link>
                 </div>
                 <div className='project3'>
                     <p>JavaScript</p>
-                    <Link to='https://ruimsmelo.github.io/shark-attack/' target='_blank'><img src={sharkAttackImg} alt='sharkattack-img' className='project-imgs'/></Link>  
+                    <Link to='https://ruimsmelo.github.io/shark-attack/' target='_blank' className='project-link'><img src={sharkAttackImg} alt='sharkattack-img' className='project-imgs'/></Link>  
                     <Link to='https://github.com/RuiMSMelo/shark-attack' target='_blank'><img src={githubImg} className='link-imgs' alt=''/></Link>
                 </div>
                 <div className='project4'>
                     <p>HTML & CSS</p>
-                    <Link to='https://ruimsmelo.github.io/NerveCenter-Homepage/' target='_blank'><img src={nerveCenterGif} alt='nervecenter-img' className='project-imgs'/></Link>
+                    <Link to='https://ruimsmelo.github.io/NerveCenter-Homepage/' target='_blank' className='project-link'><img src={nerveCenterGif} alt='nervecenter-img' className='project-imgs'/></Link>
                     <Link to='https://github.com/RuiMSMelo/NerveCenter-Homepage/tree/main' target='_blank'><img src={githubImg} className='link-imgs' alt=''/></Link>
                 </div>
             </div>
