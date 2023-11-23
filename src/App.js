3860d1d1040cd644513771d750f66207884296fa
RuiMSMelo <ruimsmelo@gmail.com>
2023-11-23 17:01:45 +0000

    added all pages and home button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Projects from './pages/Projects'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage />} />
       <Route path='/projects' element={<Projects />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contacts' element={<Contacts />} />
     </Routes>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,16 @@ import { Link } from 'react-router-dom'
 function Projects() {
     return (
         <div>
-            
+            <div>
+                <Link to='/'><h2 className='home-button'>Home</h2></Link>
+            </div>
+            <div className='allpages'>
+                <h2>Tesla</h2>
+                <h2>SoundSpace</h2>
+                <h2>Bookzone</h2>
+                <h2>Sharkattack</h2>
+                <h2>Plugin Flow</h2>
+            </div>
         </div>
     )
 }
