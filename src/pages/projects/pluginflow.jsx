e6def76c5cef97bc388b5da800095ff513be021a
RuiMSMelo <ruimsmelo@gmail.com>
2023-11-27 23:37:28 +0000

    projects almost done, icons added

--- src/pages/projects/pluginflow.jsx
+++ src/pages/projects/pluginflow.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import htmlicon from '../../img/htmlicon.svg'
+import cssicon from '../../img/cssicon.svg'
+
 
 function Pluginflow() {
     const [hoveredHome, setHoveredHome] = useState(false)
@@ -9,11 +12,28 @@ function Pluginflow() {
     }
 
     return (
-        <div className='allpages projects-div'>
+        <div className='allpages projects-div project'>
             <Link to='/'><h2 onMouseEnter={handleHoverHome} onMouseLeave={handleHoverHome} className='home-button'>
                 {hoveredHome ? 'home' : 'rui'}
             </h2></Link>
-            <p>Pluginflow</p>
+            <p>
+                During my intership with Nervecenter I was tasked to develop this homepage to their new website.
+                <br /><br />
+                made with
+                <br />
+                <div className='madewith-div'>
+                    <img src={htmlicon} className='small-icons' alt='HTML' title='HTML'/>
+                    <img src={cssicon} className='small-icons' alt='CSS' title='CSS'/>
+                </div>
+                <br /><br /><br /><br />
+                Website:
+                <br />
+                <Link to='https://ruimsmelo.github.io/NerveCenter-Homepage/' target='_blank'><span className='links-to-websites'>https://ruimsmelo.github.io/NerveCenter-Homepage/</span></Link>
+                <br />
+                Github:
+                <br />
+                <Link to='https://github.com/RuiMSMelo/NerveCenter-Homepage' target='_blank'><span className='links-to-websites'>https://github.com/RuiMSMelo/NerveCenter-Homepage</span></Link>
+            </p>
         </div>
     );
 }
--- src/pages/projects/soundspace.jsx
+++ src/pages/projects/soundspace.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import cssicon from '../../img/cssicon.svg'
+import reacticon from '../../img/reacticon.svg'
+import nodejsicon from '../../img/nodejsicon.svg'
+import mongodbicon from '../../img/mongodbicon.svg'
 
 function Soundspace() {
     const [hoveredHome, setHoveredHome] = useState(false)
@@ -9,11 +13,31 @@ function Soundspace() {
     }
 
     return (
-        <div className='allpages projects-div'>
+        <div className='allpages projects-div project'>
             <Link to='/'><h2 onMouseEnter={handleHoverHome} onMouseLeave={handleHoverHome} className='home-button'>
                 {hoveredHome ? 'home' : 'rui'}
             </h2></Link>
-            <p>Soundspace</p>
+            <p>
+            soundSpace is an innovative online platform designed to bring together individuals in the music industry, fostering a vibrant community that thrives on collaboration and connection.
+                <br /><br />
+                made with
+                <br />
+                <div className='madewith-div'>
+                    <img src={reacticon} className='small-icons' alt='React' title='React'/>
+                    <img src={cssicon} className='small-icons' alt='CSS' title='CSS'/>
+                    <img src={nodejsicon} className='small-icons' alt='NodeJS' title='NodeJS'/>
+                    <img src={mongodbicon} className='small-icons' alt='MongoDB' title='MongoDB'/>
+                </div>
+                <br /><br /><br /><br />
+                Website:
+                <br />
+                <Link to='https://lively-sprite-942da6.netlify.app/' target='_blank'><span className='links-to-websites'>https://lively-sprite-942da6.netlify.app/</span></Link>
+                <br />
+                Github:
+                <br />
+                <Link to='https://github.com/RuiMSMelo/soundSpaceFrontend' target='_blank'><span className='links-to-websites'>Frontend</span></Link>
+                <Link to='https://github.com/RuiMSMelo/soundSpaceBackend' target='_blank'><span className='links-to-websites'>Backend</span></Link>
+            </p>
         </div>
     );
 }
