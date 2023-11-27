34197f6720f2b7feb94d69f138f537bbc52f7d2f
RuiMSMelo <ruimsmelo@gmail.com>
2023-11-27 23:37:28 +0000

    projects almost done, icons added

--- src/pages/projects/bookzone.jsx
+++ src/pages/projects/bookzone.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import htmlicon from '../../img/htmlicon.svg'
+import cssicon from '../../img/cssicon.svg'
+import expressicon from '../../img/expressiconwhite.png'
+import mongodbicon from '../../img/mongodbicon.svg'
 
 function Bookzone() {
     const [hoveredHome, setHoveredHome] = useState(false)
@@ -9,11 +13,30 @@ function Bookzone() {
     }
 
     return (
-        <div className='allpages projects-div'>
+        <div className='allpages projects-div project'>
             <Link to='/'><h2 onMouseEnter={handleHoverHome} onMouseLeave={handleHoverHome} className='home-button'>
                 {hoveredHome ? 'home' : 'rui'}
             </h2></Link>
-            <p>Bookzone</p>
+            <p>
+                With just 5 days to develop this project, we decided to keep it simple but efficient and made this web application where the user can store already read and unread books.
+                <br /><br />
+                made with
+                <br />
+                <div className='madewith-div'>
+                    <img src={htmlicon} className='small-icons' alt='HTML' title='HTML'/>
+                    <img src={cssicon} className='small-icons' alt='CSS' title='CSS'/>
+                    <img src={expressicon} className='small-icons' alt='ExpressJS' title='ExpressJS'/>
+                    <img src={mongodbicon} className='small-icons' alt='MongoDB' title='MongoDB'/>
+                </div>
+                <br /><br /><br /><br />
+                Website:
+                <br />
+                <Link to='https://bookzone.adaptable.app/' target='_blank'><span className='links-to-websites'>https://bookzone.adaptable.app/</span></Link>
+                <br />
+                Github:
+                <br />
+                <Link to='https://github.com/RuiMSMelo/bookzone' target='_blank'><span className='links-to-websites'>https://github.com/RuiMSMelo/bookzone</span></Link>
+            </p>
         </div>
     );
 }
--- src/pages/projects/sharkattack.jsx
+++ src/pages/projects/sharkattack.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import javascripticon from '../../img/javascripticon.svg'
 
 function Sharkattack() {
     const [hoveredHome, setHoveredHome] = useState(false)
@@ -9,11 +10,27 @@ function Sharkattack() {
     }
 
     return (
-        <div className='allpages projects-div'>
+        <div className='allpages projects-div project'>
             <Link to='/'><h2 onMouseEnter={handleHoverHome} onMouseLeave={handleHoverHome} className='home-button'>
                 {hoveredHome ? 'home' : 'rui'}
             </h2></Link>
-            <p>Sharkattack</p>
+            <p>
+                A simple game where you control a shark, in a 2d screen, and need to eat all the food you can without getting caught by the awful, nature-disturbing scuba divers.
+                <br /><br />
+                made with
+                <br />
+                <div className='madewith-div'>
+                    <img src={javascripticon} className='small-icons' alt='HTML' title='HTML'/>
+                </div>
+                <br /><br /><br /><br />
+                Website:
+                <br />
+                <Link to='https://ruimsmelo.github.io/shark-attack/' target='_blank'><span className='links-to-websites'>https://ruimsmelo.github.io/shark-attack/</span></Link>
+                <br />
+                Github:
+                <br />
+                <Link to='https://github.com/RuiMSMelo/shark-attack' target='_blank'><span className='links-to-websites'>https://github.com/RuiMSMelo/shark-attack</span></Link>
+            </p>
         </div>
     );
 }
